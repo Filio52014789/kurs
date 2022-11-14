5094192b9dfa32d8ffb7f21f4d89ac7143843db2
1b15FM <64779077+Filio52014789@users.noreply.github.com>
2022-11-14 21:25:12 +0000

    Initial commit

---
+++ kurs.py
@@ -0,0 +1,23 @@
+print("kolejność:")
+print((2 + 2) * 2)
+print("Dzielenie:")
+print(5 / 2)
+print(5 // 2)
+print("Mnożenie")
+print(2 * 3)
+print(2 ** 3)
+print("Skrócone:")
+x = 5
+x = x + 1 
+print(x)
+x += 1 
+print(x)
+print("Konwersja:")
+# a = int(input("Podaj 1 liczbę: "))
+# b = int(input("Podaj 2 liczbę: "))
+# print(a + b)
+y = 2
+z = 2
+print(str(y) + str(z))
+del y  #usuwanie zmiennej
+print(str(y) + str(z))
