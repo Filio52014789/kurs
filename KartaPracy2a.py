5e9f827856680cc953ce0b274c475157a320c493
1b15FM <>
2022-11-15 17:20:15 +0000

    Zrobiono KartaPracy2a

---
+++ KartaPracy2a.py
@@ -0,0 +1,69 @@
+#zad 1
+# a = int(input())
+# b = int(input())
+# suma = a + b
+# if suma % 2 == 0:
+#   print("Tak")
+# else: 
+#   print("Nie")
+
+#zad 2
+# a = int(input())
+# g = int(input())
+# if (a+g)/2 > (a**g)/(1/2):
+#   print("Tak")
+# else:
+#   print("Nie")
+
+#zad 3
+# k = int(input())
+# l = int(input())
+# m = int(input())
+# if k == l:
+#   print("Tak")
+#   print(k,l)
+# elif l == m :
+#   print("Tak")
+#   print(l, m)
+# elif k == m:
+#   print("Tak")
+#   print(k, m)
+# else:
+#   print("Nie")
+
+# zad 4
+# a = str(input())
+# b = str(input())
+# c = str(input())
+# d = str(input())
+# if a < b and a < c and a < d :
+#   print("Najmniejsza z nich to " +a)
+# elif b < a and b < c and b < d :
+#   print("Najmniejsza z nich to " +b)
+# elif c < a and c < b and c < d : 
+#   print("Najmniejsza z nich to " +c)
+# elif d < a and d < b and d < c:
+#   print("Najmniejsza z nich to " +d)
+# else:
+#   print("Nie")
+
+
+#zad 5
+# a = int(input())
+# b = int(input())
+# c = int(input())
+# if a + b > c and a + c > b and b + c > a :
+#   print("Tak")
+# else:
+#   print("Nie")
+
+#zad 6
+# a = int(input())
+# b = int(input())
+# c = int(input())
+# if a*a + b*b == c*c:
+#   print("Trójkąt prostokątny")
+# elif a*a + b*b > c*c :
+#   print("Trójkąt ostrokątny")
+# elif a*a + b*b < c*c :
+#   print("Trójkąt rozwartokątny")
---
+++ lekcja3.py
@@ -0,0 +1,11 @@
+x = 10 
+y = 10
+
+if x > y :
+  print("X większe Y")
+elif x < y :
+  print("X mniejsze Y")
+elif x ==10:
+  print("X równe 10")
+else:
+  print("X równe Y")
